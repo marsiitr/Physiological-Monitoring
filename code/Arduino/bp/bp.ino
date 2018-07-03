7405795806c24f0b975b66f4d59f0770a5797e5c
MMjay87 <40452647+MMjay87@users.noreply.github.com>
2018-07-03 16:35:15 +0530

    Add files via upload

---
+++ code/Arduino/bp/bp.ino
@@ -0,0 +1,27 @@
+#include <SoftwareSerial.h>
+SoftwareSerial mySerial(10, 11); // RX, TX
+//int on = 4;
+
+void setup() 
+{Serial.begin(9600);
+//pinMode(on,OUTPUT);
+mySerial.begin(9600);}
+
+char s,c;
+void loop() 
+{
+ c = Serial.read();
+ 
+ if(c == 'o')
+ {//pinMode(on,OUTPUT);
+ //digitalWrite(on,LOW);
+ delay(700);
+ //digitalWrite(on,HIGH);
+// delay(700);
+ //digitalWrite(on,LOW);//why?
+// pinMode(on,INPUT);}
+ 
+ s = mySerial.read();
+// if (s >= 10 and s<= 60)//why?
+ Serial.write(s);
+ }}
